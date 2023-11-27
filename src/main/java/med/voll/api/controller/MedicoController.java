ad354ff0942d3143ca208130a4b621846317cefb
CarlosEduardo7700 <rm552164@fiap.com.br>
2023-11-27 20:13:12 -0300

    Adição da Paginação e Ordenação para o método de listagem de Médicos.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,6 +6,9 @@ import med.voll.api.medico.DadosParaListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,8 +28,8 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosParaListagemMedico> listar() {
-        return repository.findAll().stream().map(DadosParaListagemMedico::new).toList();
+    public Page<DadosParaListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosParaListagemMedico::new);
     }
 
 }
