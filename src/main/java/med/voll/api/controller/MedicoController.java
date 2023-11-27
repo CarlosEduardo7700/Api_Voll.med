14df2271e6a2feef93e05a57424645e9365f9e2c
CarlosEduardo7700 <rm552164@fiap.com.br>
2023-11-27 18:45:31 -0300

    Criação de uma requisição GET para a classe de Médicos.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,14 +2,14 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.medico.CredenciaisMedico;
+import med.voll.api.medico.DadosParaListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medico")
@@ -22,6 +22,11 @@ public class MedicoController {
     @Transactional
     public void Cadastrar(@RequestBody @Valid CredenciaisMedico credenciais) {
         repository.save(new Medico(credenciais));
+    }
+
+    @GetMapping
+    public List<DadosParaListagemMedico> listar() {
+        return repository.findAll().stream().map(DadosParaListagemMedico::new).toList();
     }
 
 }
