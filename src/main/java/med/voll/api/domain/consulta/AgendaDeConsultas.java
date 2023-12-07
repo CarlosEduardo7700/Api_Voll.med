7731e52959b95b93169cf3415b9a1d2a6fe82c4a
CarlosEduardo7700 <rm552164@fiap.com.br>
2023-12-06 21:34:19 -0300

    Criando a funcionalidade de agendar consultas e suas validações.

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -1,5 +1,6 @@
 package med.voll.api.domain.consulta;
 
+import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.Paciente;
@@ -23,12 +24,30 @@ public class AgendaDeConsultas {
 
     public void agendar(DadosAgendamentoConsulta dados) {
 
+        if (!pacienteRepository.existsById(dados.idPaciente())) {
+            throw new ValidacaoException("ID do paciente informado não existe!");
+        }
 
+        if (dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())) {
+            throw new ValidacaoException("ID do médico informado não existe!");
+        }
 
-        Paciente paciente = pacienteRepository.findById(dados.idPaciente()).get();
-        Medico medico = medicoRepository.findById(dados.idMedico()).get();
+        Paciente paciente = pacienteRepository.getReferenceById(dados.idPaciente());
+        Medico medico = escolherMedico(dados);
         Consulta consulta = new Consulta(null, medico, paciente, dados.data());
 
         consultaRepository.save(consulta);
+    }
+
+    private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+        if (dados.idMedico() != null) {
+            return medicoRepository.getReferenceById(dados.idMedico());
+        }
+
+        if (dados.especialidade() == null) {
+            throw new ValidacaoException("Especialidade é obrigatório no caso de um médico não ter sido escolhido.");
+        }
+
+        return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());
     }
 }
